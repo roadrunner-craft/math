e97dce31f28f63ceaf29ba45d948636cf2c4e0b6
filedesless <filedesless@gmail.com>
2020-05-09 06:13:02 -0400

    Implement Strassen's multiplication algorithm Fixes #2

--- src/matrix/matrix4.rs
+++ src/matrix/matrix4.rs
@@ -72,6 +72,93 @@ impl Matrix4 {
         }
         det
     }
+
+    fn trivial_mul(self, other: Matrix4) -> Matrix4 {
+        let mut m = Matrix4::zero();
+
+        for y in 0..self.dimensions() {
+            for x in 0..self.dimensions() {
+                let mut sum = 0.0;
+                for k in 0..self.dimensions() {
+                    sum += self[y][k] * other[k][x];
+                }
+                m[y][x] = sum;
+            }
+        }
+
+        m
+    }
+
+    // #[cfg(feature = "strassen")]
+    fn strassen_mul(self, other: Matrix4) -> Matrix4 {
+        fn mul2(a: [[f32; 2]; 2], b: [[f32; 2]; 2]) -> [[f32; 2]; 2] {
+            let mut m = [[0.0, 0.0], [0.0, 0.0]];
+
+            for y in 0..2 {
+                for x in 0..2 {
+                    let mut sum = 0.0;
+                    for k in 0..2 {
+                        sum += a[y][k] * b[k][x];
+                    }
+                    m[y][x] = sum;
+                }
+            }
+
+            m
+        }
+
+        fn add2(a: [[f32; 2]; 2], b: [[f32; 2]; 2]) -> [[f32; 2]; 2] {
+            let mut m = [[0.0, 0.0], [0.0, 0.0]];
+            for i in 0..2 {
+                for j in 0..2 {
+                    m[i][j] = a[i][j] + b[i][j];
+                }
+            }
+
+            m
+        }
+
+        fn sub2(a: [[f32; 2]; 2], b: [[f32; 2]; 2]) -> [[f32; 2]; 2] {
+            let mut m = [[0.0, 0.0], [0.0, 0.0]];
+            for i in 0..2 {
+                for j in 0..2 {
+                    m[i][j] = a[i][j] - b[i][j];
+                }
+            }
+
+            m
+        }
+
+        let a11 = [[self[0][0], self[0][1]], [self[1][0], self[1][1]]];
+        let a12 = [[self[0][2], self[0][3]], [self[1][2], self[1][3]]];
+        let a21 = [[self[2][0], self[2][1]], [self[3][0], self[3][1]]];
+        let a22 = [[self[2][2], self[2][3]], [self[3][2], self[3][3]]];
+
+        let b11 = [[other[0][0], other[0][1]], [other[1][0], other[1][1]]];
+        let b12 = [[other[0][2], other[0][3]], [other[1][2], other[1][3]]];
+        let b21 = [[other[2][0], other[2][1]], [other[3][0], other[3][1]]];
+        let b22 = [[other[2][2], other[2][3]], [other[3][2], other[3][3]]];
+
+        let m1 = mul2(add2(a11, a22), add2(b11, b22));
+        let m2 = mul2(add2(a21, a22), b11);
+        let m3 = mul2(a11, sub2(b12, b22));
+        let m4 = mul2(a22, sub2(b21, b11));
+        let m5 = mul2(add2(a11, a12), b22);
+        let m6 = mul2(sub2(a21, a11), add2(b11, b12));
+        let m7 = mul2(sub2(a12, a22), add2(b21, b22));
+
+        let c11 = add2(sub2(add2(m1, m4), m5), m7);
+        let c12 = add2(m3, m5);
+        let c21 = add2(m2, m4);
+        let c22 = add2(add2(sub2(m1, m2), m3), m6);
+
+        Matrix4([
+            [c11[0][0], c11[0][1], c12[0][0], c12[0][1]],
+            [c11[1][0], c11[1][1], c12[1][0], c12[1][1]],
+            [c21[0][0], c21[0][1], c22[0][0], c22[0][1]],
+            [c21[1][0], c21[1][1], c22[1][0], c22[1][1]],
+        ])
+    }
 }
 
 impl ops::Index<usize> for Matrix4 {
@@ -92,19 +179,7 @@ impl ops::Mul for Matrix4 {
     type Output = Matrix4;
 
     fn mul(self, other: Matrix4) -> Self::Output {
-        let mut m = Matrix4::zero();
-
-        for y in 0..self.dimensions() {
-            for x in 0..self.dimensions() {
-                let mut sum = 0.0;
-                for k in 0..self.dimensions() {
-                    sum += self[y][k] * other[k][x];
-                }
-                m[y][x] = sum;
-            }
-        }
-
-        m
+        self.strassen_mul(other)
     }
 }
 
@@ -356,5 +431,63 @@ mod tests {
         ]);
 
         assert_eq!(expects, -matrix, "Did not correctly negate matrix");
+    }
+
+    #[test]
+    fn mul_identity_returns_arg() {
+        let matrix = super::Matrix4([
+            [1.0, 2.0, 3.0, 4.0],
+            [5.0, 6.0, 7.0, 8.0],
+            [9.0, 10.0, 11.0, 12.0],
+            [13.0, 14.0, 15.0, 11.0],
+        ]);
+
+        assert_eq!(
+            matrix,
+            super::Matrix4::identity().trivial_mul(matrix),
+            "A * I should equal A"
+        );
+        assert_eq!(
+            matrix,
+            super::Matrix4::identity().strassen_mul(matrix),
+            "A * I should equal A"
+        );
+    }
+
+    #[test]
+    fn mul_zero_returns_zero() {
+        let matrix = super::Matrix4([
+            [1.0, 2.0, 3.0, 4.0],
+            [5.0, 6.0, 7.0, 8.0],
+            [9.0, 10.0, 11.0, 12.0],
+            [13.0, 14.0, 15.0, 11.0],
+        ]);
+
+        assert_eq!(
+            super::Matrix4::zero(),
+            super::Matrix4::zero().trivial_mul(matrix),
+            "A * 0 should equal 0"
+        );
+        assert_eq!(
+            super::Matrix4::zero(),
+            super::Matrix4::zero().strassen_mul(matrix),
+            "A * 0 should equal 0"
+        );
+    }
+
+    #[test]
+    fn mul_strassen_returns_like_trivial() {
+        let matrix = super::Matrix4([
+            [1.0, 2.0, 3.0, 4.0],
+            [5.0, 6.0, 7.0, 8.0],
+            [9.0, 10.0, 11.0, 12.0],
+            [13.0, 14.0, 15.0, 11.0],
+        ]);
+
+        assert_eq!(
+            matrix.trivial_mul(matrix),
+            matrix.strassen_mul(matrix),
+            "Trivial multiplication and Strassen multiplcation should return the same value"
+        );
     }
 }
