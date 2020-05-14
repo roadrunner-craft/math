d43e1c74ea1b31aa6aa1815dc85052e51edfc87e
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2020-05-14 03:53:38 -0400

    a

--- src/transform.rs
+++ src/transform.rs
@@ -89,16 +89,29 @@ impl Transform {
             ),
         );
 
-        let mut m = Matrix4::identity();
+        let m11 = 1.0;
+        let m12 = 0.0;
+        let m13 = 0.0;
+        let m14 = 0.0;
 
-        // TODO: reduce this into a single Matrix4 assignment
-        m = m * Matrix4([
-            [1.0, 0.0, 0.0, 0.0],
-            [0.0, cx, sx, 0.0],
-            [0.0, -sx, cx, 0.0],
+        let m21 = 0.0;
+        let m22 = cx;
+        let m23 = sx;
+        let m24 = 0.0;
+
+        let m31 = 0.0;
+        let m32 = -sx;
+        let m33 = cx;
+        let m34 = 0.0;
+
+        let mut m = Matrix4([
+            [m11, m12, m13, m14],
+            [m21, m22, m23, m24],
+            [m31, m32, m33, m34],
             [0.0, 0.0, 0.0, 1.0],
         ]);
 
+        // TODO: reduce this into a single Matrix4 assignment
         m = m * Matrix4([
             [cy, 0.0, -sy, 0.0],
             [0.0, 1.0, 0.0, 0.0],
